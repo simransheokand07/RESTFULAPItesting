5eb6eee75f3ce43ee1c9143490978d5fea33a737
Simran Sheokand <88535009+simransheokand07@users.noreply.github.com>
2021-11-30 12:02:46 +0530

    Add files via upload

---
+++ tests/symbolPricetokenCsvFile.py
@@ -0,0 +1,15 @@
+from FetchData import *
+import pandas as pd
+
+
+'''This function mainly used to make a csv file which stores symbols and pricetoken from symbolpricetoken_dict
+    by using pandas'''
+def symbol_and_pricetoken_in_csv(url_response):
+    symbols_list = symbol_in_list(url_response)
+    pricetoken_list = pricetoken_in_list(url_response)
+    symbolpricetoken_dict = dict(zip(symbols_list, pricetoken_list))
+    print(symbolpricetoken_dict)
+
+    df1 = pd.DataFrame(symbolpricetoken_dict.items())
+    df1.to_csv('symbolpricetokendict2.csv',index=False)
+    return df1
