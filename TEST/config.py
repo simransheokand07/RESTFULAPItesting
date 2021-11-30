32956bb1f860a68d9c369771d0513c388ab823d4
Simran Sheokand <88535009+simransheokand07@users.noreply.github.com>
2021-11-30 12:03:30 +0530

    Delete TEST directory

--- TEST/config.py
+++
@@ -1,9 +0,0 @@
-from configparser import ConfigParser
-
-
-'''This function reads the API Endpoint from properties.ini file and return sampleurl which stores the API'''
-def getconfig():
-    config = ConfigParser()
-    config.read('properties.ini')
-    sampleurl = config.get('api', 'endpoint')
-    return sampleurl
