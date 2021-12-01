e2eef84f5d218c300bb4beecbbe86541e7be45d8
Simran Sheokand <88535009+simransheokand07@users.noreply.github.com>
2021-12-01 13:01:02 +0530

    Add files via upload

---
+++ tests/allapi_convertdataintocsv.py
@@ -0,0 +1,12 @@
+from allapi_fetchdata import *
+from assertpy import assert_that
+import requests
+
+def test_check_status_code_of_csv_file():
+    df = pd.read_csv('../inputdata/allapi_symbolslist.csv', index_col=0)
+    csvlist = df.index
+    print(list(csvlist))
+
+    for item in csvlist:
+        new_response = requests.get(item)
+        assert_that(new_response.status_code == 200)
