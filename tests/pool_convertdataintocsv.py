cea13d4649f50216a0fb9c89c423a7089dd9a786
Simran Sheokand <88535009+simransheokand07@users.noreply.github.com>
2021-12-01 12:53:30 +0530

    Update pool_convertdataintocsv.py

--- tests/pool_convertdataintocsv.py
+++ tests/pool_convertdataintocsv.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from pool_comparing_symbols_from_csv_file_and_Api import *
+from pool_test_comparing_symbols_from_csv_file_and_Api import *
 
 '''This function is used to create a csv file of symbols and then symbols_list is converted into dataframe'''
 def baselinesymbols_in_new_list_and_convert_into_dataframe(get_response):
