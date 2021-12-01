3526908d2b3e2756c131e7cd6e512a7d8d0b7c70
Simran Sheokand <88535009+simransheokand07@users.noreply.github.com>
2021-12-01 15:54:22 +0530

    Update network_config.py

--- tests/network_config.py
+++ tests/network_config.py
@@ -4,6 +4,9 @@ from configparser import ConfigParser
 '''Firstly this function reads api pool_endpoint from pytest.ini file then return sampleurl which store API'''
 def getconfig():
     config = ConfigParser()
-    config.read('pytest.ini')
-    sampleurl = config.get('api','network_endpoint')
+    setup_cfg = os.path.join(root, "pytest.ini")
+    parser = config.SafeConfigParser()
+    with open(setup_cfg, "r") as f:
+        parser.readfp(f)
+    sampleurl = parser.get("api", "network_endpoint") # mandatory
     return sampleurl
