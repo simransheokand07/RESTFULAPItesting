d01b2eec34188aba26d915e3246159d1fb949cdf
Simran Sheokand <88535009+simransheokand07@users.noreply.github.com>
2021-12-01 16:38:57 +0530

    Update pool_config.py

--- tests/pool_config.py
+++ tests/pool_config.py
@@ -4,7 +4,7 @@ from configparser import ConfigParser
 '''Firstly this function reads api pool_endpoint from pytest.ini file then return sampleurl which store API'''
 def getconfig():
     config = ConfigParser()
-    config.read('../pytest.ini')
+    config.read('tests/pytest.ini')
     sampleurl = config.get('api', 'pool_endpoint')
     return sampleurl
 
