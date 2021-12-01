483d6ec869a26c4af3e76a4c4b6207f4963c640f
Simran Sheokand <88535009+simransheokand07@users.noreply.github.com>
2021-12-01 11:11:59 +0530

    Add files via upload

---
+++ tests/pool_config.py
@@ -0,0 +1,11 @@
+from configparser import ConfigParser
+
+
+'''Firstly this function reads api pool_endpoint from pytest.ini file then return sampleurl which store API'''
+def getconfig():
+    config = ConfigParser()
+    config.read('../pytest.ini')
+    sampleurl = config.get('api', 'pool_endpoint')
+    return sampleurl
+
+
