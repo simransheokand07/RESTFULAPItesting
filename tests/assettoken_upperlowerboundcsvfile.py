ef4ff9aabf591521abe634923d4b5fdec96ebbd0
Simran Sheokand <88535009+simransheokand07@users.noreply.github.com>
2021-12-01 12:36:41 +0530

    Add files via upload

---
+++ tests/assettoken_upperlowerboundcsvfile.py
@@ -0,0 +1,20 @@
+from assettokenstat_fetchdata import *
+import pandas as pd
+
+
+'''This function is used to make a csv file of upper and lower bound of pricetoken from symbolroundoff_dict '''
+def upper_bound_and_lower_bound_of_pricetoken_in_csv(url_response):
+    symbols_list = symbol_in_list(url_response)
+    pricetoken_list = pricetoken_in_list(url_response)
+    valuesRoundUp = []
+    valuesRoundDown = []
+    for number in pricetoken_list:
+        valuesRoundUp.append(math.ceil(number))
+        valuesRoundDown.append(math.floor(number))
+    roundoff_list = list(zip(valuesRoundUp, valuesRoundDown))
+    symbolroundoff_dict = dict(zip(symbols_list, roundoff_list))
+    print(symbolroundoff_dict)
+
+    df1 = pd.DataFrame(symbolroundoff_dict.items())
+    df1.to_csv("assettoken_upperlowerbound.csv", index=False)
+    return df1
